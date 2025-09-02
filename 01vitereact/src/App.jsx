740d38992690c6aab0d8168cd6b86a1fef5adb43
Harshita Thaiphule <hthaiphule@gmail.com>
2025-09-02 20:57:29 +0530

    uploaded react and vitereact

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+
+
+  return (
+    <h1>react series with vite | its harshita</h1>
+  )
+}
+
+export default App
---
+++ 1basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>react series | its hasrhita , created a raect project today!</h1>
+  );
+}
+
+export default App;
